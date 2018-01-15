1bb48e278694c3d886f84b62667f547836376196
m00n <1120399471@qq.com>
2018-01-15 15:58:01 +0800

    后台布局

--- backend/config/main.php
+++ backend/config/main.php
@@ -11,7 +11,11 @@ return [
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
-    'modules' => [],
+    'modules' => [
+        'user' => [
+            'class' => 'dektrium\user\Module',
+        ],
+    ],
     'components' => [
         'request' => [
             'csrfParam' => '_csrf-backend',
@@ -21,6 +25,13 @@ return [
             'enableAutoLogin' => true,
             'identityCookie' => ['name' => '_identity-backend', 'httpOnly' => true],
         ],
+       /* 'view' => [
+            'theme' => [
+                'pathMap' => [
+                    '@app/views' => '@vendor/yii2-adminlte-asset-master/example-views/yiisoft/yii2-advanced-app'
+                ],
+            ],
+        ],*/
         'session' => [
             // this is the name of the session cookie used for login on the backend
             'name' => 'advanced-backend',
--- common/config/main.php
+++ common/config/main.php
@@ -9,5 +9,14 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,//隐藏index.php
+             //'enableStrictParsing' => false,
+            'suffix' => '',//后缀，如果设置了此项，那么浏览器地址栏就必须带上.html后缀，否则会报404错误
+            'rules' => [
+                    //'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+            ],
+        ],
     ],
 ];
